576726a5e709e4bce6c4d5d6953e4aff2da482cb
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-03 16:31:16 +0600

    layouts

---
+++ src/Layouts/AuthLayouts.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+
+
+function AuthLayout() {
+    const location = useLocation();
+
+    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('register')
+    return (
+        <div>
+            <header>
+                {noHeaderFooter || <Navbar></Navbar>}
+            </header>
+            
+            <div className="mt-14">
+                <Outlet></Outlet>
+            </div>
+
+            <footer>
+               {noHeaderFooter ||  <Footer></Footer>}
+            </footer>
+        </div>)
+}
+
+export default AuthLayout
