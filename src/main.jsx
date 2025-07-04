941f47af5b1505dd047c1a7a12d0e7b4a9f0be37
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-04 11:51:46 +0600

    firebase

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Routes from './Routes/Routes'
+import Authprovider from './Provider/Authprovider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className="max--screen-xl mx-auto">
-      <RouterProvider router={Routes}></RouterProvider>
-    </div>
+    <Authprovider>
+      <div className="max--screen-xl mx-auto">
+        <RouterProvider router={Routes}></RouterProvider>
+      </div>
+    </Authprovider>
   </StrictMode>,
 )
