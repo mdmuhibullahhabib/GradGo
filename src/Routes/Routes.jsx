6d26d006ea470cafcee75164c524b89902ea1b6e
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-07-08 22:07:00 +0600

    routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -53,7 +53,7 @@ const Routes = createBrowserRouter([
             },
                   {
                 path: "/package/:_id",
-                element: <PrivateRoute><PackageDetails></PackageDetails>
+                element: <PrivateRoute>
 
 
 
