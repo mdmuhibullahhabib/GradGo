276ceb9303e788b79b34846557d2c64773f18233
Md Muhib Ullah Habib <mdmuhibullahhabib6348@gmail.com>
2025-08-17 23:58:01 +0600

    message

--- .history/src/Pages/Message/Message_20250817235653.jsx
+++ .history/src/Pages/Message/Message_20250817235653.jsx
@@ -132,4 +132,9 @@ const Messenger = () => {
             placeholder="Type a message..."
             className="flex-1 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
-
+          <button
+            onClick={handleSend}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+          >
+            Send
+   
